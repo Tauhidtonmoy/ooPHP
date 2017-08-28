d00d7589a6a3c0829430e8e74ebfa2ddeffde848
Tauhidtonmoy <blackheartgunner@yahoo.com>
2017-08-28 16:28:58 +0600

    First page of the project index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+          <meta charset="UTF-8">
+          <meta name="description" content="Practice">
+          <meta name="keywords" content="Object oriented PHP">
+          <meta name="author" content="Tauhidul Islam">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <header>
+        
+        </header>
+        
+        
+        <footer>
+        
+        </footer>
+    </body>
+</html>
