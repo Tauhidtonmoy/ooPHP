f09b35f62ce2efe0f5f53e28b15b6ae61507a3a4
Tauhidtonmoy <blackheartgunner@yahoo.com>
2017-08-29 09:50:13 +0600

    Added CSS to the page

--- index.php
+++ index.php
@@ -5,13 +5,29 @@
           <meta name="keywords" content="Object oriented PHP">
           <meta name="author" content="Tauhidul Islam">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+           <!-- Style Contents here -->
+        
+            <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+            <!-- jQuery library -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+            <!-- Latest compiled JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+            
+            <link rel="stylesheet" href="css/commonStyles.css">
+        
+        
+            <!-- Javascript contents here-->
     </head>
     <body>
         <header>
-            <h6> Website Under Contruction </h6> 
+            <h2> Website Under Contruction </h2> 
         </header>
-        <div>
-            <img src="img/image001.gif" alt="Site under construction">
+        <div id="mainBody">
+            <img src="img/image001.gif" alt="Site under construction"> <br/>
         </div>
         
         <footer>
